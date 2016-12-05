12a59834999743cdc2cb28fb4ef0ec8a1715c2e5
mlibby <m@mlibby.com>
2016-12-05 11:52:41 -0600

    set Befunge torus to spec size and allow for scrolling

--- mlibby/js/befunge.js
+++ mlibby/js/befunge.js
@@ -1,14 +1,9 @@
 ﻿(function () {
-    var config = {
-        maxHeight: 25,
-        maxWidth: 80
-    }
-
     var befunge = {
         x: 0,
         y: 0,
-        height: 8,
-        width: 32,
+        height: 25,
+        width: 80   ,
         rawText: "",
         parsedText: "",
     };
@@ -17,43 +12,26 @@
         var file = $("#befunge-file")[0].files[0];
         if (file) {
             var fileReader = new FileReader();
-            fileReader.onload = function (e) {
-                befunge.rawText = e.target.result;
-                parseText();
-                $("#file-name").val(file.name);
-            }
+            fileReader.onload = loadBefunge;
             fileReader.readAsText(file);
+            $("#file-name").val(file.name);
         } else {
             alert("Failed to load file");
         }
+    }
+
+    function loadBefunge(e) {
+        befunge.rawText = e.target.result;
+        parseText();
+        drawTorus();
     }
 
     function parseText() {
         var x = 0;
         var y = 0;
 
-        for (var idx = 0; idx < befunge.rawText.length; idx++) {
-            var chr = befunge.rawText.charAt(idx);
-            if (chr === "\n") {
-                x = 0;
-                y++;
-            }
-            else {
-                x++;
-            }
-
-            if (x > befunge.width && x < config.maxWidth) {
-                befunge.width = x;
-            }
-
-            if (y > befunge.height && y < config.maxHeight) {
-                befunge.height = y;
-            }
-        }
-
-        x = 0;
-        y = 0;
         befunge.parsedText = "";
+
         for (var idx = 0; idx < befunge.rawText.length; idx++) {
             var chr = befunge.rawText.charAt(idx);
             if (chr === "\n") {
@@ -71,8 +49,10 @@
                 befunge.parsedText = befunge.parsedText + chr;
             }
         }
+    }
 
-        drawTorus();
+    function getTorusId(x, y) {
+        return "cell-" + x + "-" + y;
     }
 
     function drawTorus() {
@@ -81,7 +61,7 @@
         for (var y = 0; y <= befunge.height; y++) {
             var $torusRow = $("<div class='torus-row'></div>");
             for (var x = 0; x <= befunge.width; x++) {
-                var $input = $("<input id='" + x + "-" + y + "' type='text' maxlength='1' />");
+                var $input = $("<input id='" + getTorusId(x, y) + "' type='text' maxlength='1' />");
                 var idx = (y * befunge.width) + x;
                 $input.val(befunge.parsedText.charAt(idx));
                 $torusRow.append($input);
@@ -90,10 +70,39 @@
         }
     }
 
+    function activateCurrentCell() {
+        $(".torus-row").children().removeClass("active-cell");
+        var currentCell = $("#" + getTorusId(befunge.x, befunge.y));
+        currentCell.addClass("active-cell");
+    }
+
+    function oneStep() {
+        befunge.x = befunge.x + 1;
+        if (befunge.x > befunge.width) {
+            befunge.x = 0;
+        }
+        activateCurrentCell();
+    }
+
+    function startRun() {
+        $("#befunge-run").attr("disabled", "disabled");
+        activateCurrentCell();
+        befunge.interval = setInterval(oneStep, 200);
+        $("#befunge-pause").removeAttr("disabled");
+    }
+
+    function pauseRun() {
+        $("#befunge-pause").attr("disabled", "disabled");
+        clearInterval(befunge.interval);
+        $("#befunge-run").removeAttr("disabled");
+    }
+
     $(document).ready(function () {
         if ($("h1").text() === "Befunge") {
             $("#befunge-file").change(readFile);
             drawTorus();
+            $("#befunge-run").click(startRun);
+            $("#befunge-pause").click(pauseRun);
         }
     });
 })();
